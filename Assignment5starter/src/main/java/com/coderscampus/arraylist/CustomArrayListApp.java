42e76e2915af3e94cc7cf1141c79f383b29a3719
Bianconi Ha <2BianconiHa@gmail.com>
2024-10-20 23:20:50 -0700

    Update CustomArrayListApp.java

--- Assignment5starter/src/main/java/com/coderscampus/arraylist/CustomArrayListApp.java
+++ Assignment5starter/src/main/java/com/coderscampus/arraylist/CustomArrayListApp.java
@@ -39,7 +39,7 @@ public class CustomArrayListApp {
     private static void example1() {
         CustomList<Integer> numbers = new CustomArrayList<>();
         // Add objects to CustomArrayList
-        for (int i = 1; i <= 30; i++) {
+        for (int i = 1; i <= 40; i++) {
             numbers.add(i);
         }
         // Get object at index in CustomArrayList
